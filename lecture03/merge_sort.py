42a7806f6362180221ada8ea91ac3020ef9e6bc0
lizelong <lizelong@tianyancha.com>
2022-06-21 15:23:42 +0800

    sort

---
+++ lecture03/merge_sort.py
@@ -0,0 +1,23 @@
+def merge_sort(A,a=0,b=None):
+    if b is None:
+        b = len(A)
+    if 1 < b - a:
+        c = (a + b + 1) // 2
+        merge_sort(A,a,c)
+        merge_sort(A,c,b)
+        L,R=A[a:c],A[c:b]
+        i,j=0,0
+        while a < b:
+            if(j>=len(R)) or (i < len(L) and L[i] <R[j]):
+                A[a]=L[i]
+                i=i+1
+            else:
+                A[a]=R[j]
+                j=j+1
+            a=a+1
+
+def test():
+    arr = [2,1,3]
+    merge_sort(arr, 0, 2)
+    for i in range(0,len(arr)):
+        print(arr[i])
